ef61d77215a5f1c637f5c7f34dfdc99b79601e6c
SAHIL SINGH <sahilfartyal3@gmail.com>
2023-12-16 14:33:24 +0530

    Search bar

--- Search/src/components/search.tsx
+++ Search/src/components/search.tsx
@@ -1,21 +1,41 @@
-import { useState } from "react";
+import { useState, ChangeEvent } from "react";
 import { IoIosSearch } from "react-icons/io";
 
 
 const Search = () => {
+    const [input, setInput] = useState("");
+
+    const handleChange = (event : ChangeEvent<HTMLTextAreaElement>) => {
+        setInput(event.target.value)
+        event.target.style.height = "auto";
+        event.target.style.height = `${event.target.scrollHeight}px`;
+    }
     return <div className="gtop">
         <div>
             <div className="Search">
-                <div><IoIosSearch/></div>
-                <div className = "Sbar">
-                    <input type="text" />
+                <div className="icon">
+                    <IoIosSearch/>
                 </div>
-                <div><img src="voice.png" alt="" /></div>
-                <div><img src="lense.png" alt="" /></div>
+                <div className = "Sbar">
+                    <textarea
+                    value={input}
+                    onChange={handleChange}
+                    rows={1}
+                    />
+                </div>
+                <div className="image">
+                    <img src="voice.png" alt="" />
+                </div>
+                <div className="image">
+                    <img src="lense.png" alt="" />
+                </div>
             </div>
         </div>
         <div>
-
+            <div className="Buttonbox">
+                <button>Google Search</button>
+                <button>I'm Feeling Lucky</button>
+            </div>
         </div>
     </div>
 }
