248f39d018ab72bd066d7d2f2c5cebff34f8cba7
SAHIL SINGH <sahilfartyal3@gmail.com>
2023-12-17 23:50:31 +0530

    Appbar Added

---
+++ Search/src/components/Gapps.tsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useRef } from "react";
+
+
+interface ChildProps {
+    showApps: Boolean,
+    setShowApps: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const Gapps:React.FC<ChildProps> = ({showApps, setShowApps}) => {
+    const dropdownRef = useRef<HTMLDivElement>(null);
+    useEffect(() => {
+        const handleOutsideClick: EventListener = (event) => {
+          if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+            setShowApps(!showApps);
+          }
+        };
+    
+        document.addEventListener('mousedown', handleOutsideClick);
+        return () => {
+          document.removeEventListener('mousedown', handleOutsideClick);
+        };
+      }, []);
+    return <div className="apps" ref={dropdownRef}>
+        <div className="app"><div><img src="./Apps/search.png" alt="search" /></div><div>Search</div></div>
+        <div className="app"><div><img src="./Apps/maps.png" alt="maps" /></div><div>Maps</div></div>
+        <div className="app"><div><img src="./Apps/youtube.png" alt="youtube"/></div><div>Youtube</div></div>
+        <div className="app"><div><img src="./Apps/playstore.png" alt="playstore" /></div><div>Play</div></div>
+        <div className="app"><div><img src="./Apps/doc.png" alt="news" /></div><div>News</div></div>
+        <div className="app"><div><img src="./Apps/meet.png" alt="meet" /></div><div>Meet</div></div>
+        <div className="app"><div><img src="./Apps/drive.png" alt="drive" /></div><div>Drive</div></div>
+        <div className="app"><div><img src="./Apps/photos.png" alt="photos" /></div><div>Photos</div></div>
+        <div className="app"><div><img src="./Apps/app.png" alt="apps" /></div><div>Apps</div></div>
+        <div className="app"><div><img src="./Apps/calender.png" alt="apps" /></div><div>Calender</div></div>
+    </div>
+}
+
+export default Gapps
--- Search/src/components/navbar.tsx
+++ Search/src/components/navbar.tsx
@@ -1,12 +1,25 @@
 import { PiDotsNineBold } from "react-icons/pi";
+import React, { useState , useEffect, useRef} from "react";
+import Gapps from "./Gapps";
 
-const Navbar = () => {
+const Navbar: React.FC = () => {
+    const [showApps, setShowApps] = useState(false);
+
+    const toggleManu = () => {
+        setShowApps(!showApps)
+    }
+    
     return (
         <div className="navbar">
             <div><div><a href="">Gmail</a></div>
             <div><a href="">Images</a></div></div>
-            <div id="Tool"><PiDotsNineBold/></div>
+            <div id="Tool" onClick={toggleManu}><PiDotsNineBold/></div>
             <div id="Tool"><div id="Sync">S</div></div>
+            {showApps && 
+            (<Gapps
+            showApps={showApps}
+            setShowApps={setShowApps}
+            />)}
         </div>
     )
 }
